10ec5b63d51bccda0f0d3893e7c70b98d73d5c5e
penginn-net <121443048+penginn-net@users.noreply.github.com>
2025-05-18 18:58:33 +0900

    Clip

---
+++ src/models/clip.rs
@@ -0,0 +1,42 @@
+﻿use crate::DBConnection;
+use diesel::{ExpressionMethods, QueryDsl, Selectable, SelectableHelper};
+use diesel_async::RunQueryDsl;
+
+diesel::table! {
+	#[sql_name = "clip"]
+	clip (id) {
+		id -> VarChar,
+		userId -> VarChar,
+		name -> VarChar,
+		isPublic -> Boolean,
+		description -> Nullable<VarChar>,
+		lastFetchedAt -> Nullable<Timestamp>,
+		uri -> Nullable<VarChar>,
+	}
+}
+#[derive(
+    PartialEq,
+    Eq,
+    Debug,
+    Clone,
+    diesel::Insertable,
+    diesel::Queryable,
+    Selectable,
+    diesel::QueryableByName,
+)]
+#[diesel(table_name = clip)]
+pub struct MiClip {
+    pub id: String,
+    #[diesel(column_name = "userId")]
+    pub user_id: String,
+    #[diesel(column_name = "isPublic")]
+    pub is_public: String,
+    #[diesel(column_name = "description")]
+    pub description: String,
+    #[diesel(column_name = "lastFetchedAt")]
+    pub last_fetched_at: Option<NaiveDateTime>,
+    #[diesel(column_name = "uri")]
+    pub uri: Option<String>,
+}
+impl MiClip {
+}
